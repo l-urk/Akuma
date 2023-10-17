b7dcac8d143f158a8c86f1303474b76168c617f1
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 03:46:32 -0700

    Update kill.bat

--- kill.bat
+++ kill.bat
@@ -1,10 +1,11 @@
 rmdir /s /q C:\Akuma
 rmdir /s /q C:\Amuka
 rmdir /s /q C:\Demon
-echo y| del %userprofile%\Start Menu\Programs\Startup\Akuma.lnk
-echo y| del %userprofile%\Start Menu\Programs\Startup\Demon.lnk
+rmdir /s /q C:\VvZnOpGvHw
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Akuma.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Demon.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Win.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk"
 taskkill /IM ncat.exe
 taskkill /IM ngrok.exe
-timeout /t 3
 taskkill /IM cmd.exe
-pause
