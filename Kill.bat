f8c9a1d7255d6a717cd4622de5c810ba990daaee
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-17 19:29:31 -0700

    Add files via upload

---
+++ Kill.bat
@@ -0,0 +1,11 @@
+@echo off
+echo RUN AS ADMIN FOR ANY EFFECTIVENESS
+rmdir /s /q C:\Akuma
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Akuma.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Demon.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\Win.lnk"
+echo y| del "%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk"
+taskkill /IM nc.exe
+taskkill /IM chisel.exe
+taskkill /IM cmd.exe
+pause
