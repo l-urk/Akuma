40d5765276bccac8f4c21eff1a64f4141bc03b89
L <112792604+l-urk@users.noreply.github.com>
2023-10-31 10:35:42 -0700

    Update Kill.bat

--- Kill.bat
+++ Kill.bat
@@ -1,9 +1,9 @@
 @echo off
-for /l %%i in (1,1,2) do (
 echo THIS KILL FILE IS FOR AKUMA
 echo IT MUST BE RUN AS ADMIN FOR FULL EFFECTIVE KILL
+for /l %%i in (1,1,2) do (
 echo ...press enter to skip timer...
-timeout /t 10
+timeout /t 3
 rmdir /s /q C:\Akuma
 rmdir /s /q C:\Amuka
 rmdir /s /q C:\A7uma
@@ -19,6 +19,7 @@ netsh advfirewall firewall delete rule name=".akuma"
 taskkill /IM ncat.exe /F
 taskkill /IM nc.exe /F
 taskkill /IM chisel.exe /F
-timeout /t 3
-taskkill /IM cmd.exe /F
+timeout /t 1
 )
+timeout /t 1
+taskkill /IM cmd.exe /F
